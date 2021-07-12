8faad6d8432cfd74d01fdfea0c63a21fec716b14
Salar <salar.taeb@hotmail.com>
2021-07-11 22:43:36 -0700

    learning php strings and built in methods

---
+++ workingWithStrings.php
@@ -0,0 +1,26 @@
+<?php
+$string = "Hello World";
+
+// make string lowercase example
+echo strtolower($string);
+
+// make string uppercase example
+echo strtoupper($string);
+
+// find string length
+echo strlen($string);
+
+// print out index of string
+echo $string[0];
+
+// replace part of string
+echo str_replace("Hello", "Bye", $string);
+
+// get substring of a string
+echo substr($string, 6, 3);
+
+// string functions learned so far
+// strtolower()
+// strtoupper()
+// str_replace()
+// substre()
