d571e3fd2a5800c8b90c11398a6b1e27bacd495d
Salar <salar.taeb@hotmail.com>
2021-07-11 23:18:04 -0700

    learning working with Numbers utilizng php and built in methods

---
+++ workingWIthNumbers.php
@@ -0,0 +1,33 @@
+<?php
+  $num = 10;
+  echo $num;
+
+  //incrementing a number by one
+  echo $num++;
+
+  // get absolute value of a number
+  echo abs(-100);
+
+  // take a power of a number
+  echo pow(2,6);
+   // = 2 to the power of 4 = 16
+
+  // get square rouut of a number
+  echo sqrt(144);
+
+  // find out the highest number
+  echo max(2, 10);
+
+  // find out the lowest number
+  echo min(2,10);
+
+  //round a number
+  echo round(3.7);
+
+  //round up a number
+  echo ceil(5.2);
+
+  
+  
+
+
