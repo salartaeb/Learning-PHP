52e8c24a1b81595dd82f0b33f27aeb19bb7f0a82
Salar <salar.taeb@hotmail.com>
2021-07-11 20:27:14 -0700

    learning php data types

---
+++ dataTypes.php
@@ -0,0 +1,15 @@
+<?php 
+// different types of data you can work with in php
+// String
+$stringExample = "Hello World";
+
+echo "$stringExample is a string data type";
+
+// Numbers - Integers and Floats
+$number = 14;
+echo "$number is a number data type, and is a integer because its a whole number";
+
+$float = "22.2";
+echo "$float this is a float / decimal";
+
+// booleans are pretty straightforward
